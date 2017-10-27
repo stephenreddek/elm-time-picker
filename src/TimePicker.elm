9ab46fc7db12f3af775c223fdef9e4d689a76771
Stephen Reddekopp <stephen.reddekopp@smrxt.com>
2017-10-26 22:35:25 -0500

    Setup basic format for the library

---
+++ src/TimePicker.elm
@@ -0,0 +1,81 @@
+module TimePicker exposing (Time, Period, TimePicker, Settings, Msg, init, update, view)
+
+{-| A time picker in pure elm.
+
+# Models
+@docs Time, Period, TimePicker, Settings
+
+# Update
+@docs Msg, init, update
+
+# View
+@docs view
+
+-}
+
+import Html exposing (..)
+
+
+{-| The base model for the time picker
+-}
+type TimePicker
+    = TimePicker Model
+
+
+{-| Represents AM and PM for a time
+-}
+type Period
+    = AM
+    | PM
+
+
+{-| The base way to represent time
+-}
+type alias Time =
+    { hours : Int
+    , minutes : Int
+    , seconds : Int
+    , period : Period
+    }
+
+
+{-| Contains the configuration that doesn't need to be maintained by the library
+-}
+type alias Settings =
+    { showMinutes : Bool
+    , showSeconds : Bool
+    , formatWith24Hours : Bool
+    }
+
+
+type alias Model =
+    { open : Bool
+    , value : Maybe Time
+    }
+
+
+{-| The internal messages that the picker uses to operate
+-}
+type Msg
+    = NoOp
+
+
+{-| Function for initializing a closed and empty TimePicker
+-}
+init : Maybe Time -> TimePicker
+init initialValue =
+    TimePicker (Model False initialValue)
+
+
+{-| Function to update the model when messages come
+-}
+update : Settings -> Msg -> TimePicker -> TimePicker
+update settings msg (TimePicker model) =
+    TimePicker model
+
+
+{-| Function for viewing the time picker
+-}
+view : Settings -> TimePicker -> Html Msg
+view settings (TimePicker model) =
+    div [] []
